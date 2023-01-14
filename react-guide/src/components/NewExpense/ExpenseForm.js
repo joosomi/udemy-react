ed9292c83cc97785cd9610d0dac8f796201a4c5a
JsomeDev <wnthal1211@naver.com>
2023-01-14 23:39:46 +0900

    feat: 230114SAT

--- react-guide/src/components/NewExpense/ExpenseForm.js
+++ react-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 export default function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
+  // document.getElementById('').addEventListener('click, (event) => {})
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+
+    //기술적으로 문제는 x 특정 사례에서는 문제가 발생할 수 있다. 오래된 스냅샷에 의존할수도.
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    //항상 state 업데이트할 때 최신 state을 염두에 두는 것을 보장하는 방법
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    //기본 요청을 막아서,  페이지는 새로고침x
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text"></input>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          ></input>
         </div>
-      </div>
-      <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="text" min="0.01" step="0.01"></input>
+          <input
+            type="text"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          ></input>
         </div>
-      </div>
-      <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31"></input>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          ></input>
         </div>
       </div>
       <div className="new-expense__actions">
